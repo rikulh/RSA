8c15a5129f1204855fb566bba31ad7a761831b9b
Riku <ri.arakawa.3ot0@gmail.com>
2022-03-03 22:45:48 +0900

    encodeがおかしい

--- main.cpp
+++ main.cpp
@@ -5,16 +5,18 @@
 
 using namespace std;
 
-long decode(long e, long n, long m);
-long encode(long d, long n, long c);
-long df(long p, long q, long e);
-long gcp(long s, long t);
+long long decode(long long e, long long n, long long m);
+long long encode(long long d, long long n, long long c);
+long long df(long long p, long long q, long long e);
+long long gcp(long long s, long long t);
+long long hep2dec(string text);
+string dec2hep(long long decimal);
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        long p, q, e, m;
+        long long p, q, e, m;
         cout << "p: ";
         cin >> p;
         cout << "q: ";
@@ -23,23 +25,27 @@ int main(int argc, char **argv)
         cin >> e;
         cout << "m: ";
         cin >> m;
-        long d = df(p, q, e);
-        long c = decode(e, p * q, m);
+        long long d = df(p, q, e);
+        long long c = decode(e, p * q, m);
         cout << "c: " << c << endl;
         cout << "d: " << d << endl;
         cout << "m: " << encode(d, p * q, c) << endl;
     }
     else
     {
-        string arg = argv[1];
+        string type, arg = argv[1];
+        if (argc == 3)
+        {
+            type = argv[2];
+        }
         if (arg == "--keygen")
         {
-            vector<long> primes{101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+            vector<long long> primes{101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
             std::random_device seed_gen;
             std::mt19937_64 engine(seed_gen());
-            long p = primes[engine() % primes.size()];
-            long q = primes[engine() % primes.size()];
-            long e = engine() % 1000;
+            long long p = primes[engine() % primes.size()];
+            long long q = primes[engine() % primes.size()];
+            long long e = engine() % 1000;
             while (gcp((p - 1) * (q - 1), e) != 1)
             {
                 e = engine() % 1000;
@@ -51,60 +57,131 @@ int main(int argc, char **argv)
         }
         else if (arg == "--decode")
         {
-            long n, e, m;
-            cout << "e: ";
-            cin >> e;
-            cout << "n: ";
-            cin >> n;
-            cout << "m: ";
-            cin >> m;
-            long c = decode(e, n, m);
-            cout << "c: " << c << endl;
+            if (type == "-text")
+            {
+                long long n, e;
+                string m;
+                cout << "e: ";
+                cin >> e;
+                cout << "n: ";
+                cin >> n;
+                cout << "mt: ";
+                cin >> m;
+                cout << "ct: " << dec2hep(decode(e, n, hep2dec(m))) << endl;
+            }
+            else
+            {
+                long long n, e, m;
+                cout << "e: ";
+                cin >> e;
+                cout << "n: ";
+                cin >> n;
+                cout << "m: ";
+                cin >> m;
+                cout << "c: " << decode(e, n, m) << endl;
+            }
         }
         else if (arg == "--encode")
         {
-            long p, q, e, c;
-            cout << "p: ";
-            cin >> p;
-            cout << "q: ";
-            cin >> q;
-            cout << "e: ";
-            cin >> e;
-            cout << "c: ";
-            cin >> c;
-            long m = encode(df(p, q, e), p * q, c);
-            cout << "m: " << m << endl;
+            if (type == "-text")
+            {
+                long long p, q, e;
+                string c;
+                cout << "p: ";
+                cin >> p;
+                cout << "q: ";
+                cin >> q;
+                cout << "e: ";
+                cin >> e;
+                cout << "ct: ";
+                cin >> c;
+                cout << hep2dec(c);
+                cout << "mt: " << dec2hep(encode(df(p, q, e), p * q, hep2dec(c))) << endl;
+            }
+            else
+            {
+                long long p, q, e, c;
+                cout << "p: ";
+                cin >> p;
+                cout << "q: ";
+                cin >> q;
+                cout << "e: ";
+                cin >> e;
+                cout << "c: ";
+                cin >> c;
+                cout << "m: " << encode(df(p, q, e), p * q, c) << endl;
+            }
         }
     }
     return 0;
 }
 
-long df(long p, long q, long e)
+long long hep2dec(string text)
 {
-    long a = e;
-    long b = (p - 1) * (q - 1);
-    vector<long> rs{1,b/a};
-	long r,c;
-	r = b % a;
-	b = a;
-	a = r;
-	for (int i = 2;b % a != 0;i++) {
-		rs.push_back(rs[i-2] + b / a * rs[i-1]);
-		r = b % a;
-		b = a;
-		a = r;
-	}
-	c = rs[rs.size() - 1];
-	if (c % (p - 1) * (q - 1) != 1) {
-		c = (p - 1) * (q - 1) - c;
-	}
-	return c;
+    long long result;
+    long long twenties = 28;
+    vector<int> decs;
+    for (int i = text.size() - 1; i >= 0; i--)
+    {
+        char digit = text[i];
+        int ascii = (int)digit - 96;
+        decs.push_back(ascii);
+    }
+    result = decs[0];
+    for (int i = 1; i < decs.size(); i++)
+    {
+        result += twenties * decs[decs.size() - i];
+        twenties *= twenties;
+    }
+    return result;
 }
 
-long gcp(long s, long t)
+string dec2hep(long long decimal)
 {
-    long a = s;
-    long b = t;
+    long long orig = decimal;
+    vector<int> decs;
+    while (orig != 0)
+    {
+        decs.push_back(orig % 28);
+        orig /= 28;
+    }
+    char heps[decs.size()];
+    for (int i = 1; i <= decs.size(); i++)
+    {
+        heps[decs.size() - i] = decs[i - 1] + 96;
+    }
+    string result(heps,decs.size());
+    return result;
+}
+
+long long df(long long p, long long q, long long e)
+{
+    long long a = e;
+    long long b = (p - 1) * (q - 1);
+    vector<long long> rs{1, b / a};
+    long long r, c;
+    r = b % a;
+    b = a;
+    a = r;
+    for (int i = 2; b % a != 0; i++)
+    {
+        rs.push_back(rs[i - 2] + b / a * rs[i - 1]);
+        r = b % a;
+        b = a;
+        a = r;
+    }
+    c = rs[rs.size() - 1];
+    if (c % (p - 1) * (q - 1) != 1)
+    {
+        c = (p - 1) * (q - 1) - c;
+    }
+    return c;
+}
+
+long long gcp(long long s, long long t)
+{
+    long long a = s;
+    long long b = t;
     while (true)
     {
         if (a == b)
@@ -115,30 +192,30 @@ long gcp(long s, long t)
     }
 }
 
-long decode(long e, long n, long m)
+long long decode(long long e, long long n, long long m)
 {
-    vector<long> e_binary;
-    long e_tmp = e;
-    vector<long> m_mods;
-    long me_mod = 0;
+    vector<long long> e_binary;
+    long long e_tmp = e;
+    vector<long long> m_mods;
+    long long me_mod = 0;
     while (e_tmp != 0)
     {
         e_binary.push_back((e_tmp % 2 == 0) ? false : true);
         e_tmp /= 2;
     }
     m_mods.push_back(m % n);
-    for (long i = 1; i < e_binary.size(); i++)
+    for (long long i = 1; i < e_binary.size(); i++)
     {
         m_mods.push_back((m_mods[i - 1] * m_mods[i - 1]) % n);
     }
-    for (long i = 0; me_mod == 0; i++)
+    for (long long i = 0; me_mod == 0; i++)
     {
         if (e_binary[i])
         {
             me_mod = m_mods[i];
         }
     }
-    for (long i = 1; i < e_binary.size(); i++)
+    for (long long i = 1; i < e_binary.size(); i++)
     {
         if (e_binary[i])
         {
@@ -152,30 +229,30 @@ long decode(long e, long n, long m)
     return me_mod;
 }
 
-long encode(long d, long n, long c)
+long long encode(long long d, long long n, long long c)
 {
-    vector<long> d_binary;
-    long d_tmp = d;
-    vector<long> c_mods;
-    long cd_mod = 0;
+    vector<long long> d_binary;
+    long long d_tmp = d;
+    vector<long long> c_mods;
+    long long cd_mod = 0;
     while (d_tmp != 0)
     {
         d_binary.push_back((d_tmp % 2 == 0) ? false : true);
         d_tmp /= 2;
     }
     c_mods.push_back(c % n);
-    for (long i = 1; i < d_binary.size(); i++)
+    for (long long i = 1; i < d_binary.size(); i++)
     {
         c_mods.push_back((c_mods[i - 1] * c_mods[i - 1]) % n);
     }
-    for (long i = 0; cd_mod == 0; i++)
+    for (long long i = 0; cd_mod == 0; i++)
     {
         if (d_binary[i])
         {
             cd_mod = c_mods[i];
         }
     }
-    for (long i = 1; i < d_binary.size(); i++)
+    for (long long i = 1; i < d_binary.size(); i++)
     {
         if (d_binary[i])
         {
